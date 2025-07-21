1b5848f599183bba05b201f61157920bbafd4085
Saniya Kumari <saniyakumari1976@gmail.com>
2025-07-21 23:36:55 +0530

    Initial commit for Expenzo fullstack app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className="text-4xl text-green-500 font-bold">Tailwind is Working!</h1>
+<h1 className="text-4xl text-green-500 font-bold">Tailwind is Working!</h1>
+
+      
+    </>
+  )
+}
+
+export default App
