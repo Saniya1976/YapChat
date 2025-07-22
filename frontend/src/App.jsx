8fdf23f1e9c94157778ebd2542d88653290d3dee
Saniya Kumari <saniyakumari1976@gmail.com>
2025-07-22 17:01:57 +0530

    Add routing and authentication structure to the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+ import './App.css'
+import {
+  browserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+import Home from './pages/Home'
+import Income from './pages/Income'
+import Expense from './pages/Expense'
+import Login from './pages/auth/login'
+import SignUp from './pages/auth/signup'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-     <h1 className="text-4xl text-green-500 font-bold">Tailwind is Working!</h1>
-<h1 className="text-4xl text-green-500 font-bold">Tailwind is Working!</h1>
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/Income" element={<Income />} />
+          <Route path="/Expense" element={<Expense />} />
+        </Routes>
+      </Router>
       
     </>
   )
 }
 
 export default App
+const Root=()=>{
+  const isAuthenticated=localStorage.getItem('token');
+  return(
+    <>
+    {isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />}
+    </>
+  )
+}
---
+++ frontend/src/pages/Auth/Login.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const login = () => {
+  return (
+  <>
+    <h1>Login Page</h1>
+    <p>Please enter your credentials to log in.</p>
+    <form>
+      <div className="form-group">
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" required />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" required />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </>
+  )
+}
+
+export default login
