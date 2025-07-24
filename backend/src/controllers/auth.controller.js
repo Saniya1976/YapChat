25ce20701adcb534174cf1d27e843bbed43515b2
Saniya Kumari <saniyakumari1976@gmail.com>
2025-07-25 01:30:25 +0530

    Implement authentication routes and database connection

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export async function signup(req, res) {
+  res.send('Welcome to the YapChat API');
+}
+export async function login(req, res) {
+  res.send('Login Route');
+}
+export async function logout(req, res) {
+  res.send('Logout Route');
+}   
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
+import { connectDB } from './lib/db.js'
 dotenv.config()
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+connectDB();
 
 app.get('/api/auth/signup', (req, res) => {
   res.send('Welcome to the YapChat API')
