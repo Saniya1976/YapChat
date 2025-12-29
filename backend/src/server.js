9ec983c2ded6f241d8198049bae80b9f27c31f84
Saniya1976 <saniyakumari1976@gmail.com>
2025-12-29 10:16:25 +0530

    yapchat maintainance

--- backend/src/server.js
+++ backend/src/server.js
@@ -39,12 +39,13 @@
 });
 
     if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../public")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../public", "index.html"));
   });
 }
+
     const PORT = process.env.PORT || 5001
     app.listen(PORT, () => {
       console.log(` Server running on port http://localhost:${PORT}`)
