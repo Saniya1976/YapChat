ad646e27953b8b9558ce973c4dfcb510f80968bd
Saniya1976 <saniyakumari1976@gmail.com>
2025-12-30 12:34:21 +0530

    redeploy on render

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const connectDB= async()=>{
     try{
-        const connect=await mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
+        const connect=await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected', `${connect.connection.host}`);
     }catch(err){
         console.error('MongoDB connection error:', err);
